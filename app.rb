21fe7caa09512370515a15a87284fdb99475dded
Vitaliy Vinnikov <itmastertm@gmail.com>
2020-09-06 05:34:47 -0700

    add routes to app.rb

--- app.rb
+++ app.rb
@@ -2,3 +2,32 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+
+# Роуты для страниц
+get '/' do
+	erb :index		
+end
+
+get '/index' do
+  erb :index
+end
+
+get '/something' do
+  erb :something
+end
+
+get '/products' do
+  erb :products
+end
+
+get '/partners' do
+  erb :partners
+end
+
+get '/vacancies' do
+  erb :vacancies
+end
+
+get '/contacts' do
+  erb :contacts
+end
